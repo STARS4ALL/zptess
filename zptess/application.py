55c3e53c4e3e989387626be00f710793774894a5
Rafael Gonzalez <astrorafael@yahoo.es>
2020-02-25 23:48:56 +0100

    simplifying

--- zptess/application.py
+++ zptess/application.py
@@ -10,44 +10,21 @@
 
 from __future__ import division, absolute_import
 
-import os.path
-
 # ---------------
 # Twisted imports
 # ---------------
 
-from zope.interface    import implementer, Interface
-from twisted.internet  import task, reactor, defer
-from twisted.persisted import sob
-from twisted.python    import components
-from twisted.application.service import IService, Service, MultiService, Process
+from twisted.application.service import Application
 
 #--------------
 # local imports
 # -------------
 
-from . import STATS_SERVICE, TEST_PHOTOMETER_SERVICE, REF_PHOTOMETER_SERVICE
-
-from zptess.logger     import sysLogInfo,  startLogging
-from zptess.config     import VERSION_STRING, read_options
+from zptess            import STATS_SERVICE, TEST_PHOTOMETER_SERVICE, REF_PHOTOMETER_SERVICE
+from zptess.logger     import startLogging
+from zptess.config     import read_options
 from zptess.stats      import StatsService
 from zptess.photometer import PhotometerService
-
-def Application(name, uid=None, gid=None):
-    """
-    Return a compound class.
-    Return an object supporting the L{IService}, L{IReloadable}, L{IServiceCollection},
-    L{IProcess} and L{sob.IPersistable} interfaces, with the given
-    parameters. Always access the return value by explicit casting to
-    one of the interfaces.
-    """
-    ret = components.Componentized()
-    availableComponents = [MultiService(), Process(uid, gid),
-                           sob.Persistent(ret, name)]
-    for comp in availableComponents:
-        ret.addComponent(comp, ignoreClass=1)
-    IService(ret).setName(name)
-    return ret  
 
 # ====
 # Main
